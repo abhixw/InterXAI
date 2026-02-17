03a52297e4f9bc074458b3e2c4798119028fc0e4
Abhinav Shrimali <abhinav@Abhinavs-MacBook-Air.local>
2026-02-18 02:01:04 +0530

    Serve React frontend from Django (built assets and config)

--- core/core/settings.py
+++ core/core/settings.py
@@ -173,9 +173,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Optional: makes files smaller & cached
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
